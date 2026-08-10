00f22019e081138dc0c1bd7280b29924fd670fd7
mashencia <mashencia.malic@gmail.com>
2026-08-10 08:58:48 -0400

    Added Header and TripList componenets

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="title">OBD II Dashboard</div>
+            <div className="carInfo">Mazda 3</div>
+            <div className="userLogo">M M</div>
+        </div>
+    );
+}
---
+++ client/src/components/TripList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import useTrips from '../hooks/useTrips';
+import { Trip } from '../types'
+
+interface TripListProps {
+    onTripSelect: (trip: Trip) => void
+}
+
+function TripList({ onTripSelect }: TripListProps) {
+    const { loading, trips, error } = useTrips();
+
+    // Loading State
+    if(loading) return <div>Loading...</div>;
+
+    // Error State
+    if(error) return <div>{error}</div>;
+
+    // Success State
+    return (
+        <div className ="trip-list">
+            {trips.map((trip: Trip) => (
+                <div key={trip.tripId} className="trip-item" onClick={() => onTripSelect(trip)}>
+                    <div>{new Date(trip.startTime).toLocaleDateString()}</div>
+                    <div>RPM {trip.avgRpm}</div>
+                    <div>{trip.avgVehicleSpeed}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default TripList;
